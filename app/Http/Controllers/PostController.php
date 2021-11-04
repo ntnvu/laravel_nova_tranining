fc77a92fb3daab0d69e738ed353efe1d0f02af4f
root <root@vultr.guest>
2021-11-04 12:06:07 +0000

    Passport API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,26 +1,125 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-use Exception;
 use App\Models\Post;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
-    public function show($slug)
+    public function index()
     {
-        $post = Post::where('slug', $slug)->first();
+        $posts = auth()->user()->posts;
+
+        return response()->json([
+            'success' => true,
+            'data' => $posts
+        ]);
+    }
+
+    public function showBySlug(Request $request)
+    {
+        $post = Post::where('slug', $request->slug)->first();
         if ($post) {
             $imagePath = Storage::url($post->featured_image);
-            return view('post', [
-                'post' => $post,
-                'imagePath' => $imagePath,
+//            return view('post', [
+//                'post' => $post,
+//                'imagePath' => $imagePath,
+//            ]);
+            return response()->json([
+                'success' => true,
+                'data' => $post->toArray()
             ]);
         }
 
         // No match was found
         abort(404);
+    }
+
+    public function show($id)
+    {
+        $post = auth()->user()->posts()->find($id);
+
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $post->toArray()
+        ], 400);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+
+        if (auth()->user()->posts()->save($post))
+            return response()->json([
+                'success' => true,
+                'data' => $post->toArray()
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not added'
+            ], 500);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = auth()->user()->posts()->find($id);
+
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 400);
+        }
+
+        $updated = $post->fill($request->all())->save();
+
+        if ($updated)
+            return response()->json([
+                'success' => true
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post can not be updated'
+            ], 500);
+    }
+
+    public function destroy($id)
+    {
+        $post = auth()->user()->posts()->find($id);
+
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 400);
+        }
+
+        if ($post->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post can not be deleted'
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Route::post('getPostBySlug', 'PostController@showBySlug');
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::resource('posts', PostController::class);
+    Route::post('getPostBySlug', 'PostController@showBySlug');
 });
